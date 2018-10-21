8fc7f5cabe0d4f21b8c5501e0a89930e91b51112
Marcin Rakoczy <mrakoczy@wp.pl>
2018-10-21 16:12:23 +0200

    Add AssetModel class and GlobalConfigurationa

---
+++ WebExperience.Test/Models/AssetModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebExperience.Test.Models
+{
+    public class AssetModels
+    {
+        public int Id { get; set; }
+        public string Asset { get; set; }
+        public string Country { get; set; }
+        public string MimeType { get; set; }
+    }
+}

fc962eb9cb0fc90a86491837511aecf07e4c5751
Marcin Rakoczy <mrakoczy@wp.pl>
2018-10-22 13:16:48 +0200

    Add API

--- WebExperience.Test/Controllers/AssetController.cs
+++ WebExperience.Test/Controllers/AssetController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Web;
 using System.Web.Http;
 using WebExperience.Test.Models;
 
@@ -17,5 +19,83 @@ namespace WebExperience.Test.Controllers
         // Any data repository is permitted
         // Use a client MVVM framework
 
+        private ApplicationDbContext _context;
+
+        public AssetController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/asets
+        public IEnumerable<Asset> GetAssets()
+        {
+            return _context.Assets.ToList();
+        }
+
+        // GET /api/asets/1
+        public Asset GetAsset(int id)
+        {
+            var asset = _context.Assets.SingleOrDefault(a => a.Id == id);
+
+            if (asset == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return asset;
+        }
+
+        // POST /api/assets
+        [HttpPost]
+        public Asset CreateAsset(Asset asset)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Assets.Add(asset);
+            _context.SaveChanges();
+
+            return asset;
+        }
+
+        // PUT /api/assets/1
+        [HttpPut]
+        public void UpdateAssets(int id, Asset asset)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var assetInDb = _context.Assets.SingleOrDefault(a => a.Id == id);
+
+            if (assetInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            assetInDb.AssetName = asset.AssetName;
+            assetInDb.Country = asset.Country;
+            assetInDb.MimeType = asset.MimeType;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/assets/1
+        [HttpDelete]
+        public void DeleteAsset(int id)
+        {
+            var assetInDb = _context.Assets.SingleOrDefault(a => a.Id == id);
+
+            if (assetInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Assets.Remove(assetInDb);
+            _context.SaveChanges();
+        }
     }
 }

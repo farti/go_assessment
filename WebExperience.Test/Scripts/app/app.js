f24919763fb299c46378502aede99256f685dc44
Marcin Rakoczy <mrakoczy@wp.pl>
2018-10-29 07:14:04 +0100

    Add Angular

---
+++ WebExperience.Test/Scripts/app/app.js
@@ -0,0 +1,30 @@
+﻿var myApp = angular.module('myApp', [
+    'ngRoute',
+    'EmpControllers'
+]);
+
+EmpApp.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/list', {
+            templateUrl: 'asset/list.html',
+            controller: 'ListController'
+        }).
+        when('/create', {
+            templateUrl: 'asset/edit.html',
+            controller: 'EditController'
+        }).
+        when('/edit/:id', {
+            templateUrl: 'asset/edit.html',
+            controller: 'EditController'
+        }).
+        when('/delete/:id', {
+            templateUrl: 'asset/delete.html',
+            controller: 'DeleteController'
+        }).
+        otherwise({
+            redirectTo: '/list',
+            controller: 'ListController'
+        });
+
+}]);
